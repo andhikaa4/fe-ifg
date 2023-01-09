053fcf9669501ea1efc5e788a16848a0b80c6243
andhikaa4 <110803805+andhikaa4@users.noreply.github.com>
2023-01-09 17:01:06 +0700

    fe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './component/Pages/MenuUser/Index';
+import Index2 from './component/Pages/MenuPelanggan/Index';
+import Index3 from './component/Pages/MenuProduk/Index';
+import Index4 from './component/Pages/DetailTransaksi/Index';
+import Index5 from './component/Pages/PembCicTetap/Index';
+import Index6 from './component/Pages/TrxCicTetap/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/user' element={<Index/>} />
+        <Route path='/pelanggan' element={<Index2/>} />
+        <Route path='/produk' element={<Index3/>} />
+        <Route path='/detail-transaksi' element={<Index4/>} />
+        <Route path='/pembayaran-cicilan' element={<Index5/>} />
+        <Route path='/transaksi-cicilan' element={<Index6/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
